b77163148b5eba55fd56a1a578601aa390668ca9
zoerod9 <zoe.rod9767@gmail.com>
2025-02-05 18:15:56 -0600

    inital commit

---
+++ src/page.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function Hello() {
+    const [count, setCount] = useState(0)
+    return( 
+        <>
+<p>hello, test</p>
+</>
+    )
+}
+export default Hello
